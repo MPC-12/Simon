5e75847bfabf3b83b9dc28e3e5ec8b0e38b6238f
MPC-12 <88403061+MPC-12@users.noreply.github.com>
2021-09-02 18:47:03 -0500

    Archivos correctos

--- game.js
+++ game.js
@@ -3,28 +3,29 @@ var gamePatern = [];
 var userClickedPattern = [];
 var level = 0;
 
-$(document).keydown( function(event) {
+$(".start").click(function() {
   if (level == 0) {
     nextSequence();
   }
 });
 
-$(".btn").click(function (id){
+$(".btn").click(function (){
   var usenChosenColour = this.id;
   userClickedPattern.push(usenChosenColour);
   pushButton(usenChosenColour);
   if ((userClickedPattern[userClickedPattern.length -1] != gamePatern[userClickedPattern.length -1]) && (gamePatern.length!=0)) {
     level = 0;
-      $("#level-title")[0].innerHTML = "Press A Key to Start";
+      $("#level-title")[0].innerHTML = "Press This Button to Start";
       var audio = new Audio("sounds/wrong.mp3");
       audio.play();
+      $("body")[0].classList.add("game-over");
+      setTimeout( function() {
+        $("body")[0].classList.remove("game-over");    }, 300);
       userClickedPattern.length = 0;
       gamePatern.length = 0;
   } else if (userClickedPattern.length==gamePatern.length) {
     setTimeout( function() {
     userClickedPattern.length = 0;
-    console.log(gamePatern);
-    console.log(userClickedPattern);
     nextSequence();
   }, 1000);
 } else {
